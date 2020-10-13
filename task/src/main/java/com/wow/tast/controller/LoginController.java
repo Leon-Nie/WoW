c8e60d08e14338c4797eda705e10e14993b9ddd4
Leon-Nie <348142073@qq.com>
2020-10-13 22:57:28 +0800

    init tast

---
+++ task/src/main/java/com/wow/tast/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.wow.tast.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@RestController
+public class LoginController {
+
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String Login(){
+
+        log.info("successfully");
+        return "login";
+    }
+}
